5be6f38edca648fc349739755323ec18288327f7
Zakhele <prince.feela2905@gmail.com>
2025-08-21 02:30:15 +0200

    .

--- alx-movie-app/next.config.js
+++ alx-movie-app/next.config.js
@@ -1,13 +1,17 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-      },
-    ],
+    domains: ['m.media-amazon.com'],
   },
 };
 
-module.exports = nextConfig;
+export default withPWA({
+  ...nextConfig
+})
